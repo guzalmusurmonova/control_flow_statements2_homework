0c256c5ba625dbf526b59e45af410b151a7578b2
guzalmusurmonova <burxonovsherzod0025@gmail.com>
2023-04-17 03:45:22 -0700

    05

--- if05.py
+++ if05.py
@@ -6,4 +6,20 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1=n%10
+    x2=n%100//10
+    x3=n%1000//100
+    x4=n%10000//1000
+    x5=n//100000
+    z=x1
+    if z<x2:
+        z=x2
+    if z<x3:
+        z=x3
+    if z<x4:
+        z=x4
+    if z<x5:
+        z=x5
+
+    return z 
+print(main(23546))
